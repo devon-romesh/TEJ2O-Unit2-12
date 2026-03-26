a3f6443f2c1e74b186e43f1f137136a0f63b50e0
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-26 12:18:01 -0400

    Comments

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Mar 2026
+ * This program will find the distance of an object and find if the the distance is far or not
 */
 
 basic.showString('Hello, World!')
