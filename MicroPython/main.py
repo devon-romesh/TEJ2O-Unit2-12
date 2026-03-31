8b20d48081e054e38c6846ddb68846f49cca6634
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-03-31 16:52:54 +0000

    coded working version 9.0

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -10,8 +10,7 @@ import neopixel
 
 class HCSR04:
     # this class abstracts out the functionality of the HC-SR04 and
-    # returns distance in mm
-
+    #   returns distance in mm
     # Trig: pin 1
     # Echo: pin 2
     def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
@@ -26,50 +25,41 @@ class HCSR04:
             mosi=self.trigger_pin,
             miso=self.echo_pin,
         )
-
         pre = 0
         post = 0
         k = -1
         length = 500
-
         resp = bytearray(length)
         resp[0] = 0xFF
-
         spi.write_readinto(resp, resp)
-
         # find first non zero value
         try:
             i, value = next((ind, v) for ind, v in enumerate(resp) if v)
         except StopIteration:
             i = -1
-
         if i > 0:
             pre = bin(value).count("1")
-
             # find first non full high value afterwards
             try:
                 k, value = next(
                     (ind, v)
-                    for ind, v in enumerate(resp[i:length - 2])
+                    for ind, v in enumerate(resp[i : length - 2])
                     if resp[i + ind + 1] == 0
                 )
                 post = bin(value).count("1") if k else 0
                 k = k + i
             except StopIteration:
                 i = -1
-
         dist = -1 if i < 0 else round(((pre + (k - i) * 8.0 + post) * 8 * 0.172) / 2)
         return dist
 
 
 # startup
 sonar = HCSR04()
-display.show(Image.HAPPY) 
-
+display.show(Image.HAPPY)
 myNeopixelStrip = neopixel.NeoPixel(pin16, 4)
 myNeopixelStrip.clear()
 myNeopixelStrip.show()
-
 
 # display distance
 while True:
@@ -84,15 +74,13 @@ while True:
             myNeopixelStrip[1] = (255, 0, 0)
             myNeopixelStrip[2] = (255, 0, 0)
             myNeopixelStrip[3] = (255, 0, 0)
-            myNeopixelStrip.show()
 
         # if not
         else:
-            myNeopixelStrip[0] = (0, 0, 255)
-            myNeopixelStrip[1] = (0, 0, 255)
-            myNeopixelStrip[2] = (0, 0, 255)
-            myNeopixelStrip[3] = (0, 0, 255)
-            myNeopixelStrip.show()
+            myNeopixelStrip[0] = (0, 255, 0)
+            myNeopixelStrip[1] = (0, 255, 0)
+            myNeopixelStrip[2] = (0, 255, 0)
+            myNeopixelStrip[3] = (0, 255, 0)
 
         # show neopixels
         myNeopixelStrip.show()
@@ -103,3 +91,4 @@ while True:
         myNeopixelStrip.clear()
         myNeopixelStrip.show()
         display.show(Image.HAPPY)
+        
